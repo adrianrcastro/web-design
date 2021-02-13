8486e447d1b1a106050b665182885d5232a7d2da
adrianrcastro <60081283+adrianrcastro@users.noreply.github.com>
2021-02-12 20:18:45 -0400

    Add files via upload

---
+++ diseñoWeb-Adrian/main.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById("menuIcon");
+const slideoutMenu = document.getElementById("slide-menu");
+
+menuIcon.addEventListener('click', function () {
+  if (slideoutMenu.style.opacity == "1") {
+    slideoutMenu.style.opacity = '0';
+    slideoutMenu.style.pointerEvents = 'none';
+  } else {
+    slideoutMenu.style.opacity = '1';
+    slideoutMenu.style.pointerEvents = 'auto';
+  }
+})
